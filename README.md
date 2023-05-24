17a49f34a8dbb6d955d5c7956d1975a553deb4d1
袁雅婷 <yuanyating@yuanyatingdeMacBook-Pro.local>
2023-05-25 00:20:50 +0800

    feature: initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Formilyjs
+
+- SchemaField 组件是专门用于解析 JSON-Schema 动态渲染表单的组件
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import FormSimple from './components/FormSimple'
+import FormComplicated from './components/FormComplicated'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* <FormSimple /> */}
+      <FormComplicated />
+    </>
+  )
+}
+
+export default App
