c52a48920ee857e66b07b32c90be47aec3a415b1
袁雅婷 <yuanyating@yuanyatingdeMacBook-Pro.local>
2023-05-26 03:30:52 +0800

    feat: 初始值

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       {/* <FormSimple /> */}
-      <FormComplicated />
+      {/* <FormComplicated /> */}
       <Hive />
     </>
   );
--- src/components/Hive.tsx
+++ src/components/Hive.tsx
@@ -1,5 +1,5 @@
-import { createForm } from "@formily/core";
-import { createSchemaField } from "@formily/react";
+import { createForm } from '@formily/core'
+import { createSchemaField } from '@formily/react'
 import {
   Form,
   FormButtonGroup,
@@ -7,7 +7,8 @@ import {
   Input,
   Submit,
   Select,
-} from "@formily/antd";
+} from '@formily/antd'
+import { useEffect } from 'react'
 
 /**
  * 实现一个可联动的Hive
@@ -15,12 +16,16 @@ import {
 
 const form = createForm({
   validateFirst: true,
-});
+  effects: (form) => {
+    console.log('form', form)
+  }
+})
 
 const SelectRemote = ({ ...props }) => {
-  console.log("props", props);
-  return <Select />;
-};
+  console.log('props', props)
+  
+  return <Select  />
+}
 
 const SchemaField = createSchemaField({
   components: {
@@ -28,46 +33,54 @@ const SchemaField = createSchemaField({
     Input,
     SelectRemote,
   },
-});
+
+})
 
 const schema = {
-  type: "object",
+  type: 'object',
   properties: {
     db: {
-      type: "string",
-      title: "库名",
-      "x-decorator": "FormItem",
-      "x-component": "SelectRemote",
-      "x-validator": [],
-      "x-component-props": {},
-      "x-decorator-props": {},
-      "x-designable-id": "fg3pfwbxge0",
-      "x-index": 0,
-      "x-data": {
-        url: "xxx",
-        params: {
-          form: "cc",
-          system: "xx",
+      type: 'string',
+      title: '库名',
+      'x-decorator': 'FormItem',
+      'x-component': 'SelectRemote',
+      'x-validator': [],
+      'x-component-props': {
+        custom: {
+          url: 'xxx',
+          params: {
+            form: 'cc',
+            system: 'xx',
+          },
         },
       },
-      name: "db",
+      'x-decorator-props': {},
+      'x-designable-id': 'fg3pfwbxge0',
+      'x-index': 0,
+      
+      name: 'db',
     },
     table: {
-      type: "string",
-      title: "表名",
-      "x-decorator": "FormItem",
-      "x-component": "Input",
-      "x-validator": [],
-      "x-component-props": {},
-      "x-decorator-props": {},
-      "x-designable-id": "ljywjjxqo0w",
-      "x-index": 1,
-      name: "table",
+      type: 'string',
+      title: '表名',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-validator': [],
+      'x-component-props': {},
+      'x-decorator-props': {},
+      'x-designable-id': 'ljywjjxqo0w',
+      'x-index': 1,
+      name: 'table',
     },
   },
-};
+}
 
 export default () => {
+
+  useEffect(() => {
+    form.setInitialValues({db: 'abc'})
+  }, [])
+
   return (
     <Form form={form} labelCol={5} wrapperCol={16} onAutoSubmit={console.log}>
       <SchemaField schema={schema} />
@@ -77,5 +90,5 @@ export default () => {
         </Submit>
       </FormButtonGroup.FormItem>
     </Form>
-  );
-};
+  )
+}
